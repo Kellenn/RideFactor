43ca5234f1ef13453d1c6746f2835da93a92ed3f
Kellen <70085049+Kellenn@users.noreply.github.com>
2025-05-06 15:28:20 -0600

    Add loading logic

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,15 @@ function processScore(score) {
     const precipitationElement = document.querySelector("#precipitation");
     const windElement = document.querySelector("#wind");
     const daytimeElement = document.querySelector("#daytime");
+    const arcText = document.querySelector("#arc-text");
+    const arcLoadingSvg = document.querySelector("#arc")
 
     temperatureElement.textContent = `Temperature: ${score.temperature.value}°${score.temperature.unit}`;
     precipitationElement.textContent = `Precipitation: ${score.precipitation}%`;
     windElement.textContent = `Wind: ${score.windSpeed}`;
     daytimeElement.textContent = `Daytime: ${score.daytime ? "Yes" : "No"}`;
+    arcText.style.opacity = 1;
+    arcLoadingSvg.style.backgroundImage = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
